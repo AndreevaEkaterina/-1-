52bf2917aae432420e6e9f2c5126dfd6319060cf
Ekaterina <114247065+AndreevaEkaterina@users.noreply.github.com>
2022-11-27 13:35:44 +0800

    решила задачу

--- Program.cs
+++ Program.cs
@@ -6,3 +6,22 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] RightSize (string[] array, int num)
+{
+    string result = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < num) result = result + array[i] + " ";
+    }
+    string[] newArr = result.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    return newArr;
+}
+
+Console.Clear();
+Console.WriteLine("Введите слова через пробел: ");
+string mass = (Console.ReadLine()!);
+int n = 4;
+string[] arr = mass.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Console.WriteLine($"Слова с количеством букв 3 и меньше: {string.Join(", ", RightSize(arr, n))}");
